aa15edd803d82b3020e5b1fbcdf648f95cc700da
Prima Aulia Gusta <primaulia@gmail.com>
2017-06-28 11:47:36 +0800

    latest search by brand with keyup

--- js/dom-google-shopping.js
+++ js/dom-google-shopping.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   // THIS LINE STARTS THE PARSING OF THE JSON
-  var json = {}
-
   fetch('../products.json')
   .then(res => res.json())
   .then((data) => {
     // DO NOT REMOVE ANYTHING BEFORE THIS LINE
-    console.log('Checkout this JSON! ', data)
+    // console.log('Checkout this JSON! ', data.items)
 
     // START YOUR CODE FROM HERE AND HERE ONLY
     // ALL YOUR INTERNAL FUNCTIONS SHOULD START FROM HERE AND HERE ONLY TOO
@@ -20,6 +18,55 @@ document.addEventListener('DOMContentLoaded', function () {
       var listItem = document.createElement('li')
       listItem.textContent = item.product.title
       shoppingList.appendChild(listItem)
+    })
+
+    var button = document.querySelector('button')
+    button.addEventListener('click', addToCart)
+
+    function addToCart () {
+      var shoppingList = document.querySelectorAll('#shopping-list li')
+      if (!shoppingList.length) {
+        alert('shopping list is empty')
+        return // need to return, so the function stops running
+      }
+
+      var cartList = document.querySelector('#cart-list')
+      var lastItem = shoppingList[shoppingList.length - 1]
+      cartList.appendChild(lastItem)
+    }
+
+    // search by brand
+    var searchBrand = document.querySelector('#searchBrand')
+    // if i put the searchValue here, it's always empty on getItemsByBrand
+
+    function getItemsByBrand (eachItem) {
+      // if i put here, searchValue is always new
+      var searchValue = searchBrand.value
+      var brandNameInData = eachItem.product.brand.toLowerCase()
+
+      // should only returns condition based on each item
+      // items[i].product.brand === brand
+      // console.log('each item brand name is', brandNameInData)
+      // console.log('searchValue is ', searchValue)
+      // console.log(brandNameInData === searchValue)
+
+      return brandNameInData === searchValue
+    }
+
+    function filterItems(keyword, callback)
+
+    searchBrand.addEventListener('keyup', function () {
+      // console.log(items)
+
+      var filteredItems = items.filter(getItemsByBrand)
+      shoppingList.innerHTML = ''
+      filteredItems.forEach(function (item) {
+        console.log(item);
+        // console.log(shoppingList);
+        var listItem = document.createElement('li')
+        listItem.textContent = item.product.title
+        shoppingList.appendChild(listItem)
+      })
     })
 
     // DO NOT REMOVE ANYTHING AFTER THIS LINE
